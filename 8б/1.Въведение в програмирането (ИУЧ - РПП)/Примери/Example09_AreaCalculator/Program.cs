0b0725011e84594ff6664116936046597af54590
ahmed <ahmed@atami.no>
2026-02-09 09:40:37 +0200

    add contetfor proccess

--- 8б/1.Въведение в програмирането (ИУЧ - РПП)/Примери/Example09_AreaCalculator/Program.cs
+++ 8б/1.Въведение в програмирането (ИУЧ - РПП)/Примери/Example09_AreaCalculator/Program.cs
@@ -1,28 +1,13 @@
 using System;
 
+i = i + 1
+i = 2 + 1  // i++
+
 class Program {
-    static void Main() {
-        Console.WriteLine("=== Калкулатор за площ на правоъгълник ===");
-        
-        Console.Write("Въведете дължината (см): ");
-        double length = double.Parse(Console.ReadLine());
-        
-        Console.Write("Въведете ширината (см): ");
-        double width = double.Parse(Console.ReadLine());
-        
-        double area = length * width;
-        
-        Console.WriteLine($"\nПлощта на правоъгълника е: {area} кв. см");
-        
-        if (area > 100) {
-            Console.WriteLine("Това е голям правоъгълник!");
-        } else if (area > 50) {
-            Console.WriteLine("Това е среден правоъгълник.");
-        } else {
-            Console.WriteLine("Това е малък правоъгълник.");
+   static void Main() {
+       for (int i = 0; i < 5; i++)   {
+          Console.WriteLine(i);
+          Console.ReadLine();
         }
-        
-        Console.WriteLine("\nНатисни Enter за изход...");
-        Console.ReadLine();
-    }
+   }
 }
